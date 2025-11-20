696d3e245d092cdcf9d770d5fddfd314439a40e6
Noah Cammelbeeck <thenoahbentzen@gmail.com>
2025-11-20 13:50:03 +0100

    adding toggle switch

--- js/dark.js
+++ js/dark.js
@@ -1,8 +1,18 @@
-const root = document.querySelector("html");
-const btn = document.querySelector("#themeBtn");
+// const root = document.querySelector("html");
+// const btn = document.querySelector("#themeBtn");
 
-function toggleTheme() {
-  root.classList.toggle("dark");
+// function toggleTheme() {
+//   root.classList.toggle("dark");
+// }
+
+// btn.addEventListener("click", toggleTheme);
+
+const html = document.querySelector("html");
+const toggleSwitch = document.querySelector("#theme-toggle");
+
+function toggleSwitchFn(){
+  const isDark = event.target.checked === true;
+  html.classList.toggle("dark", isDark)
 }
 
-btn.addEventListener("click", toggleTheme);
+toggleSwitch?.addEventListener("change", toggleSwitchFn)
